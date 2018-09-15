60cfdba83e96ae16c317be0a6e153af6e7458d1d
andreil <andreil499@gmail.com>
2018-09-15 17:32:23 +0300

    Обновлены используемые ROM

--- fpga/.test/ROM2/rom2.v
+++ fpga/.test/ROM2/rom2.v
@@ -88,9 +88,9 @@ module rom2 (
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
 `ifdef NO_PLI
-		altsyncram_component.init_file = "./ROMs/ROM2-210.rif"
+		altsyncram_component.init_file = "./ROMs/ROM2-321.rif"
 `else
-		altsyncram_component.init_file = "./ROMs/ROM2-210.hex"
+		altsyncram_component.init_file = "./ROMs/ROM2-321.hex"
 `endif
 ,
 		altsyncram_component.intended_device_family = "Cyclone V",
@@ -127,7 +127,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "./ROMs/ROM2-210.hex"
+// Retrieval info: PRIVATE: MIFfilename STRING "./ROMs/ROM2-321.hex"
 // Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "65536"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -142,7 +142,7 @@ endmodule
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "./ROMs/ROM2-210.hex"
+// Retrieval info: CONSTANT: INIT_FILE STRING "./ROMs/ROM2-321.hex"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
